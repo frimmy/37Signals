253fe6ea676c136238fd6fb1cb3413810a118694
Adrian_F <adrian.frimpong@gmail.com>
2013-11-01 11:49:24 -0500

    Updated javascript for present day and optimized jQuery calls

--- application.js
+++ application.js
@@ -1,40 +1,47 @@
 $(function () {
-    $('#box-bc').hover(
-    	function () {
-            
-            $('#modal').hide();
-            $('#bc').show();
-        
-        }, function () {
-            
-    		$('#modal').show();
-            $('#bc').hide();
-        
+    $('#box-bc').hover(function () {
+        $('#modal').toggle();
+        $('#bc').toggle();
         });
+    // , function () {
+            
+    // 		$('#modal').show();
+    //         $('#bc').hide();
+        
+    //     });
 
-    $('#box-hr').hover(
-    	function () {
-            
-            $('#modal').hide();
-            $('#hr').show();
+    $('#box-hr').hover(function () {
+    	$('#modal').toggle();
+        $('#bc').toggle();
+        });            
+      //       $('#modal').hide();
+      //       $('#hr').show();
         
-        }, function () {
+      //   }, function () {
             
-    		$('#modal').show();
-            $('#hr').hide();
+    		// $('#modal').show();
+      //       $('#hr').hide();
         
-        });
+      //   });
 
-    $('#box-cf').hover(
-    	function () {
-            
-            $('#modal').hide();
-            $('#cf').show();
-        
-        }, function () {
-            
-    		$('#modal').show();
-            $('#cf').hide();
-        
+    $('#box-cf').hover(function () {
+    	$('#modal').toggle();
+        $('#bc').toggle();
         });
+            
+      //       $('#modal').hide();
+      //       $('#cf').show();
+        
+      //   }, function () {
+            
+    		// $('#modal').show();
+      //       $('#cf').hide();
+        
+      //   });
+    // Display current day of the week
+    var daynum = new Date().getDay();
+    var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	var day = dayArray[daynum];
+
+    $('#greeting').html("Happy "+day);
 });
