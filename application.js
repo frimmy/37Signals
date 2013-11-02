c226a95a74b9b6b307cb125d031d6685a604b103
Adrian_F <adrian.frimpong@gmail.com>
2013-11-02 14:46:36 -0500

    Optimized javascript file

--- application.js
+++ application.js
@@ -1,43 +1,16 @@
 $(function () {
     $('#box-bc').hover(function () {
-        $('#modal').toggle();
-        $('#bc').toggle();
+        $('#modal','#bc').toggle();
         });
-    // , function () {
-            
-    // 		$('#modal').show();
-    //         $('#bc').hide();
-        
-    //     });
 
     $('#box-hr').hover(function () {
-    	$('#modal').toggle();
-        $('#bc').toggle();
+    	$('#modal','#bc').toggle();
         });            
-      //       $('#modal').hide();
-      //       $('#hr').show();
-        
-      //   }, function () {
-            
-    		// $('#modal').show();
-      //       $('#hr').hide();
-        
-      //   });
 
     $('#box-cf').hover(function () {
-    	$('#modal').toggle();
-        $('#bc').toggle();
+    	$('#modal','#bc').toggle();
         });
             
-      //       $('#modal').hide();
-      //       $('#cf').show();
-        
-      //   }, function () {
-            
-    		// $('#modal').show();
-      //       $('#cf').hide();
-        
-      //   });
     // Display current day of the week
     var daynum = new Date().getDay();
     var dayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
